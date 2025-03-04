e9794399c6759bf57e044a266f7b9256b6c3367a
Maxim Kemp <maximakemp@gmail.com>
2025-03-04 16:09:43 -0500

    update backend structure: add dotenv configuration, modify database connection, and create user schema; update package.json for main entry and dependencies

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const usersRouter = require("./users");
 require("dotenv").config();
+const db = require("./db");
 
 const app = express();
 const port = process.env.PORT || 5002;
