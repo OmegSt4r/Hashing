59c3656aeba13b2ac372164c2e812f391a8b2427
Evan <ebogedin@oakland.edu>
2025-03-07 17:05:38 -0500

    add dump file, ping-database.js for testing and added drop down menu for selected encryption type

---
+++ backend/ping-database.js
@@ -0,0 +1,45 @@
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Define your credentials using environment variables
+const dbConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // Optional if 3306 is the default port
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  ...dbConfig,
+  authPlugins: {
+    sha256_password: () => (data, cb) => {
+      cb(null, Buffer.from(dbConfig.password));
+    },
+  },
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to the database");
+
+  // Ping the database
+  connection.ping((err) => {
+    if (err) {
+      console.error("Error pinging the database:", err);
+    } else {
+      console.log("Database pinged successfully");
+    }
+
+    // Close the connection
+    connection.end();
+  });
+});
--- schema.sql
+++ schema.sql
@@ -7,5 +7,6 @@ CREATE TABLE user_info (
   user_id INT,
   u_password VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
+  p_encryption_type VARCHAR(255) NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
